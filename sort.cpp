d31e60ea9aa7c0bd2fb3f04421113bb457c185d8
Skalhaxor <skalex_989@hotmail.com>
2014-03-17 18:44:33 -0400

    Convert tabs to spaces

--- sort.cpp
+++ sort.cpp
@@ -9,57 +9,57 @@ void sort(float *data, int length);
 
 int main()
 {
-	/*float* sortedArray = new float[ARRAY_LENGTH];
-	for (int i = 0; i < ARRAY_LENGTH; ++i) {
-		sortedArray[i] = i;
-	}
+    /*float* sortedArray = new float[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        sortedArray[i] = i;
+    }
 
-	float* backwards = new float[ARRAY_LENGTH];
-	for (int i = 0; i < ARRAY_LENGTH; ++i) {
-		backwards[i] = ARRAY_LENGTH-i;
-	}*/
+    float* backwards = new float[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        backwards[i] = ARRAY_LENGTH-i;
+    }*/
 
-	float* random = new float[ARRAY_LENGTH];
-	for (int i = 0; i < ARRAY_LENGTH; ++i) {
-		random[i] = (float)rand();
-	}
+    float* random = new float[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        random[i] = (float)rand();
+    }
 
-	double time = omp_get_wtime();
-	sort(random, ARRAY_LENGTH);
-	time = omp_get_wtime() - time;
+    double time = omp_get_wtime();
+    sort(random, ARRAY_LENGTH);
+    time = omp_get_wtime() - time;
 
-	cin.get();
+    cin.get();
 }
 
 void sort(float *data, int length) {
     // Initialize //////////////////////////////////////////////////////
-	int  numThreads       = omp_get_max_threads();
-	int* dataStartIndexes = new int[numThreads];
-	int* dataLengths      = new int[numThreads];
-	int  dataLength       = length / numThreads;
+    int  numThreads       = omp_get_max_threads();
+    int* dataStartIndexes = new int[numThreads];
+    int* dataLengths      = new int[numThreads];
+    int  dataLength       = length / numThreads;
 
     // Partition Data //////////////////////////////////////////////////
-	for (int i = 0; i < numThreads - 1; ++i) {
-		dataLengths[i]      = dataLength;
-		dataStartIndexes[i] = dataLength*i;
-	}
+    for (int i = 0; i < numThreads - 1; ++i) {
+        dataLengths[i]      = dataLength;
+        dataStartIndexes[i] = dataLength*i;
+    }
 
-	// make the last thread handle the left over data
-    // (will be >= dataLength and < 2*dataLength)
-	dataStartIndexes[numThreads - 1] = dataLength * (numThreads - 1);
-	dataLengths[numThreads - 1]      = length - dataStartIndexes[numThreads - 1];
-	
+    // make the last thread handle the left over data (will be >= dataLength and < 2*dataLength)
+    dataStartIndexes[numThreads - 1] = dataLength * (numThreads - 1);
+    dataLengths[numThreads - 1]      = length - dataStartIndexes[numThreads - 1];
+    
     // Startup Threads /////////////////////////////////////////////////
-	// If a SHARED variable in a parallel region is read by the threads executing the region,
-	// but not written to by any of the threads, then specify that variable to be FIRSTPRIVATE instead of SHARED.
-	// This avoids accessing the variable by dereferencing a pointer, and avoids cache conflicts. 
+    // If a SHARED variable in a parallel region is read by the threads executing the region,
+    // but not written to by any of the threads, then specify that variable to be FIRSTPRIVATE instead
+    // of SHARED. This avoids accessing the variable by dereferencing a pointer, and avoids cache
+    // conflicts. 
 #pragma omp parallel firstprivate(dataStartIndexes, dataLengths)
-	{
+    {
         // this is just a test
-		int threadId = omp_get_thread_num();
+        int threadId = omp_get_thread_num();
 #pragma omp critical 
-		cout << "I'm Thread " << threadId << " and handle data from " << dataStartIndexes[threadId] <<
-			" to " << dataStartIndexes[threadId] + dataLengths[threadId] - 1 << endl;
+        cout << "I'm Thread " << threadId << " and handle data from " << dataStartIndexes[threadId] <<
+            " to " << dataStartIndexes[threadId] + dataLengths[threadId] - 1 << endl;
 
         // Quicksort ///////////////////////////////////////////////////
 
@@ -86,5 +86,5 @@ void sort(float *data, int length) {
 
 
         // Consolidate /////////////////////////////////////////////////
-	}
+    }
 }
