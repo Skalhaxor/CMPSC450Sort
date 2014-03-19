1f44e213c618825795f5e2563ea650426434631e
JKillian <jasonkillian35@gmail.com>
2014-03-19 15:03:29 -0400

    Almost working correctly, need to fix bug in multimerge

--- floatsort.cpp
+++ floatsort.cpp
@@ -4,8 +4,6 @@
 #include <algorithm>
 #include <omp.h>
 
-using namespace std;
-
 #define ARRAY_LENGTH 50       // length of array to be sorted
 
 // structure to put in the multimerge priority queue
@@ -27,7 +25,32 @@ struct PriorityInfo
     }
 };
 
-void sort(float *data, int length);
+struct WriteBuffer
+{
+    int index;
+    int* segmentLengths;
+    float* data;
+    WriteBuffer() {
+        index = 0;
+        writeNum = 0;
+    }
+
+    void init(int length, int numThreads) {
+        data = new float[length];
+        segmentLengths = new int[numThreads];
+    }
+
+    void finalizeWrite(int length) {
+        segmentLengths[writeNum] = length;
+        index += length;
+        writeNum++;
+    }
+
+private:
+    int writeNum;
+};
+
+float* sort(float *data, int length);
 void multimerge(float* arrays[], const int lengths[], const int numArrays, float newArray[],
                                                                          const int newLength);
 
@@ -49,22 +72,48 @@ int main()
     }
 
     double time = omp_get_wtime();
-    sort(random, ARRAY_LENGTH);
+    float* sortedData = sort(random, ARRAY_LENGTH);
     time = omp_get_wtime() - time;
 
-    cout << "Press any key to continue...";
-    cin.get();
+    std::cout << std::endl << "Sorted Data: " << std::endl;
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        std::cout << sortedData[i] << std::endl;
+    }
+
+    std::cout << "Press any key to continue...\n";
+
+    float* test = new float[5];
+    test[0] = 23; // start of first partition
+    test[1] = 55;
+    test[2] = 29;   // start of second partition
+    test[3] = 102;
+    test[4] = 20; // start of third and fourth partition. Third partition has no length
+
+    int lengths[] = { 2, 2, 0, 1 };
+    float sorted[5];
+    float* arrays[] = { test, test + 2, test + 4, test + 4 };
+    multimerge(arrays, lengths, 4, sorted, 5);
+    for (int i = 0; i < 5; ++i)
+        std::cout << sorted[i] << ", ";
+
+    std::cin.get();
 }
 
-void sort(float *data, int length) {
+float* sort(float *data, int length) {
     // Initialize //////////////////////////////////////////////////////
-    int     numThreads       = omp_get_max_threads();
-    int     numThreadsSq     = numThreads * numThreads;
-    int*    dataStartIndexes = new int[numThreads];
-    int*    dataLengths      = new int[numThreads];
-    int     segmentLength    = length / numThreads;
-    float*  samples          = new float[numThreads];
-    float*  pivots           = new float[numThreads + 1];
+    int          numThreads       = omp_get_max_threads();
+    int          numThreadsSq     = numThreads * numThreads;
+    int*         dataStartIndexes = new int[numThreads];
+    int*         dataLengths      = new int[numThreads];
+    int          segmentLength    = length / numThreads;
+    float*       samples          = new float[numThreads];
+    float*       pivots           = new float[numThreads + 1];
+    WriteBuffer* dataInBuffs      = new WriteBuffer[numThreads];
+    float**      sortedDatas      = new float*[numThreads];
+    float*       finalData        = new float[length];
+    for (int i = 0; i < numThreads; ++i) {
+        dataInBuffs[i].init(length, numThreads);
+    }
 
     // Partition Data //////////////////////////////////////////////////
     for (int i = 0; i < numThreads - 1; ++i) {
@@ -89,20 +138,20 @@ void sort(float *data, int length) {
 
         // this is just a test for debugging
 #pragma omp critical 
-        cout << "I'm Thread " << threadId << " and handle data from " << dataStartIndexes[threadId] <<
-            " to " << dataStartIndexes[threadId] + dataLengths[threadId] - 1 << endl;
+        std::cout << "I'm Thread " << threadId << " and handle data from " << dataStartIndexes[threadId] <<
+            " to " << dataStartIndexes[threadId] + dataLengths[threadId] - 1 << std::endl;
 
         // Quicksort ///////////////////////////////////////////////////
         // should each thread operate on the main array but only in its own secgment?
         // or should each thread copy its data segment to its own local private array?
         // either way, we should just find a library function to do this prob
-        vector<float> dataVec(data + dataStart, data + dataStart + dataLength);
-        sort(dataVec.begin(), dataVec.end());
+        std::vector<float> dataVec(data + dataStart, data + dataStart + dataLength);
+        std::sort(dataVec.begin(), dataVec.end());
 #pragma omp critical 
         {
-            cout << "Thread " << threadId << " sort:\n";
+            std::cout << "Thread " << threadId << " sort:\n";
             for (int i = 0; i < dataLength; ++i)
-                cout << dataVec[i] << endl;
+                std::cout << dataVec[i] << std::endl;
         }
         
         // Choose Sample Data //////////////////////////////////////////
@@ -119,9 +168,9 @@ void sort(float *data, int length) {
 
 #pragma omp single
         {
-            cout << "The samples: " << endl;
+            std::cout << "The samples: " << std::endl;
             for (int i = 0; i < numThreadsSq; ++i)
-                cout << samples[i] << endl;
+                std::cout << samples[i] << std::endl;
             // Multimerge Samples //////////////////////////////////////
             int*    lengths       = new int[numThreads];
             float** arrays        = new float*[numThreads];
@@ -132,9 +181,9 @@ void sort(float *data, int length) {
             }
             multimerge(arrays, lengths, numThreads, sortedSamples, numThreadsSq);
 
-            cout << "Sorted samples: " << endl;
+            std::cout << "Sorted samples: " << std::endl;
             for (int i = 0; i < numThreadsSq; ++i)
-                cout << sortedSamples[i] << endl;
+                std::cout << sortedSamples[i] << std::endl;
 
             // Choose Pivots ///////////////////////////////////////////
             // choose p - 1 pivots from the merged samples, non-randomly
@@ -143,10 +192,13 @@ void sort(float *data, int length) {
                 float pivot = sortedSamples[numThreads * i];
                 pivots[i] = pivot;
             }
-            pivots[numThreads] = sortedSamples[numThreadsSq - 1];
-            cout << "The pivots: " << endl;
+            // largest value of samples may not be largest max value in data
+            // so make the endpoint of the last partition the largest possible float value
+            pivots[numThreads] = FLT_MAX;
+
+            std::cout << "The pivots: " << std::endl;
             for (int i = 0; i <= numThreads; ++i)
-                cout << pivots[i] << endl;
+                std::cout << pivots[i] << std::endl;
         }
 
 
@@ -155,8 +207,8 @@ void sort(float *data, int length) {
         // Our pivots look like this:
         //   | |    |     |      |
         //   xxxxxxxxxxxxxxxxxxxxx
-        int* partitionStartIndices = new int[numThreads];
-        int* partitionLengths      = new int[numThreads];
+        std::vector<int> partitionStartIndices(numThreads);
+        std::vector<int> partitionLengths(numThreads);
         int  currPartition         = 0;
 
         partitionStartIndices[0] = 0;
@@ -164,7 +216,7 @@ void sort(float *data, int length) {
 
         for (int i = 0; i < dataLength; ++i) {
             float dataPoint = dataVec[i];
-
+ 
             if (pivots[currPartition] <= dataPoint && dataPoint < pivots[currPartition+1] ) {
                 partitionLengths[currPartition]++;
             }
@@ -179,20 +231,74 @@ void sort(float *data, int length) {
 
 #pragma omp critical
         {
-            cout << "For thread " << threadId << ":\n";
+            std::cout << "For thread " << threadId << ":\n";
             for (int i = 0; i < numThreads; ++i) {
-                cout << " Partition " << i << " starts at index " << partitionStartIndices[i]
-                    << " and is " << partitionLengths[i] << " long." << endl;
+                std::cout << " Partition " << i << " starts at index " << partitionStartIndices[i]
+                    << " and is " << partitionLengths[i] << " long." << std::endl;
             }
         }
-
+#pragma omp barrier
 
         // Multimerge Classes //////////////////////////////// //////////
         // the i-th thread multimerges all the i-th classes of each thread's partitioned data
+#pragma omp critical
+        {
+            //write data from each partition to corresponding thread
+            for (int i = 0; i < numThreads; ++i) {
+                WriteBuffer* buf = dataInBuffs + i;
+                int dataToWriteLength = partitionLengths[i];
+                int dataStartIndex = partitionStartIndices[i];
+                for (int j = 0; j < dataToWriteLength; ++j) {
+                    buf->data[j + buf->index] = dataVec[j + dataStartIndex];
+                }
+                buf->finalizeWrite(dataToWriteLength);
+            }
+        }
 
+#pragma omp barrier
+
+#pragma omp critical
+        {
+            std::cout << "For thread " << threadId << ":\n";
+            for (int i = 0; i < dataInBuffs[threadId].index; ++i) {
+                std::cout << dataInBuffs[threadId].data[i] << std::endl;
+            }
+        }
+
+        float* dataPointer = dataInBuffs[threadId].data;
+        float** arrays = new float*[numThreads];
+        float*  sortedData = new float[dataInBuffs[threadId].index];
+        for (int i = 0; i < numThreads; ++i) {
+            arrays[i] = dataPointer;
+            dataPointer += dataInBuffs[threadId].segmentLengths[i];
+        }
+        multimerge(arrays, dataInBuffs[threadId].segmentLengths, numThreads, sortedData, dataInBuffs[threadId].index);
+        sortedDatas[threadId] = sortedData;
+
+#pragma omp critical
+        {
+            std::cout << "For thread " << threadId << ":\n";
+            for (int i = 0; i < dataInBuffs[threadId].index; ++i) {
+                std::cout << sortedData[i] << std::endl;
+            }
+        }
+
+#pragma omp barrier
 
         // Consolidate /////////////////////////////////////////////////
-    }
+#pragma omp single
+        {
+            int pos = 0;
+            for (int i = 0; i < numThreads; ++i) {
+                for (int j = 0; j < dataInBuffs[i].index; ++j) {
+                    finalData[pos + j] = sortedDatas[i][j];
+                }
+                pos += dataInBuffs[i].index;
+            }
+        }
+    } // end omp parallel
+
+    return finalData;
 }
 
 // takes in multiple arrays of already sorted data and sorts them together into one new array
@@ -207,7 +313,7 @@ void multimerge(float* arrays[], const int lengths[], const int numArrays, float
     int* indexes       = new int[numArrays];       // current index into each array in arrays; parallel
                                                    //   with arrays
     int  newArrayIndex = 0;                        // current index into newArray
-    priority_queue<PriorityInfo> curPriorities;    // priority queue of current smallest values from
+    std::priority_queue<PriorityInfo> curPriorities;    // priority queue of current smallest values from
                                                    //   each array in arrays
 
     // initialize indexes and insert into curPriorities the first value from each array in arrays
